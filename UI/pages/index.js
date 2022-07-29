e22e8ae9895e5111dcbaf533318adae134210357
Kaustubh Ratna <kaustubhratna@gmail.com>
2022-07-29 14:48:15 +0530

    UI init

---
+++ UI/pages/index.js
@@ -0,0 +1,250 @@
+
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import Web3 from 'web3'
+
+import styles from '../styles/Home.module.css'
+import 'bulma/css/bulma.css'
+
+export default function Home() {
+  const [web3, setWeb3] = useState()
+  const [address, setAddress] = useState()
+  const [lcContract, setLcContract] = useState()
+  const [lotteryPot, setLotteryPot] = useState()
+  const [lotteryPlayers, setPlayers] = useState([])
+  const [lotteryHistory, setLotteryHistory] = useState([])
+  const [lotteryId, setLotteryId] = useState()
+  const [error, setError] = useState('')
+  const [successMsg, setSuccessMsg] = useState('')
+
+  useEffect(() => {
+    updateState()
+  }, [lcContract])
+
+  const updateState = () => {
+    if (lcContract) getPot()
+    if (lcContract) getPlayers()
+    if (lcContract) getLotteryId()
+  }
+
+  const getPot = async () => {
+    const pot = await lcContract.methods.getBalance().call()
+    setLotteryPot(web3.utils.fromWei(pot, 'ether'))
+  }
+
+  const getPlayers = async () => {
+    const players = await lcContract.methods.getPlayers().call()
+    setPlayers(players)
+  }
+
+  const getHistory = async (id) => {
+    setLotteryHistory([])
+    for (let i = parseInt(id); i > 0; i--) {
+      const winnerAddress = await lcContract.methods.lotteryHistory(i).call()
+      const historyObj = {}
+      historyObj.id = i
+      historyObj.address = winnerAddress
+      setLotteryHistory(lotteryHistory => [...lotteryHistory, historyObj])
+    }
+  }
+
+  const getLotteryId = async () => {
+    const lotteryId = await lcContract.methods.lotteryId().call()
+    setLotteryId(lotteryId)
+    await getHistory(lotteryId)
+  }
+
+  const enterLotteryHandler = async () => {
+    setError('')
+    setSuccessMsg('')
+    try {
+      await lcContract.methods.enter().send({
+        from: address,
+        value: '15000000000000000',
+        gas: 300000,
+        gasPrice: null
+      })
+      updateState()
+    } catch(err) {
+      setError(err.message)
+    }
+  }
+
+  const pickWinnerHandler = async () => {
+    setError('')
+    setSuccessMsg('')
+    console.log(`address from pick winner :: ${address}`)
+    try {
+      await lcContract.methods.pickWinner().send({
+        from: address,
+        gas: 300000,
+        gasPrice: null
+      })
+    } catch(err) {
+      setError(err.message)
+    }
+  }
+
+  const payWinnerHandler = async () => {
+    setError('')
+    setSuccessMsg('')
+    try {
+      await lcContract.methods.payWinner().send({
+        from: address,
+        gas: 300000,
+        gasPrice: null
+      })
+      console.log(`lottery id :: ${lotteryId}`)
+      const winnerAddress = await lcContract.methods.lotteryHistory(lotteryId).call()
+      setSuccessMsg(`The winner is ${winnerAddress}`)
+      updateState()
+    } catch(err) {
+      setError(err.message)
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    setError('')
+    setSuccessMsg('')
+    /* check if MetaMask is installed */
+    if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+      try {
+        /* request wallet connection */
+        await window.ethereum.request({ method: "eth_requestAccounts"})
+        /* create web3 instance & set to state */
+        const web3 = new Web3(window.ethereum)
+        /* set web3 instance in React state */
+        setWeb3(web3)
+        /* get list of accounts */
+        const accounts = await web3.eth.getAccounts()
+        /* set account 1 to React state */
+        setAddress(accounts[0])
+
+        /* create local contract copy */
+        const lc = lotteryContract(web3)
+        setLcContract(lc)
+
+        window.ethereum.on('accountsChanged', async () => {
+          const accounts = await web3.eth.getAccounts()
+          console.log(accounts[0])
+          /* set account 1 to React state */
+          setAddress(accounts[0])
+        })
+      } catch(err) {
+        setError(err.message)
+      }
+    } else {
+      /* MetaMask is not installed */
+      console.log("Please install MetaMask")
+    }
+  }
+
+
+  return (
+    <div>
+      <Head>
+        <title>GenZ Lottery</title>
+        <meta name="description" content="An Ethereum Lottery dApp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <nav className="navbar is-dark mt-0 mb-4">
+          <div className="container">
+            <div className="navbar-brand">
+              <h1>GenZ Lottery</h1>
+            </div>
+            <div className="navbar-end">
+              <button onClick={connectWalletHandler} className="button is-link">Connect Wallet</button>
+            </div>
+          </div>
+        </nav>
+        <div className="container">
+          <section className="mx-0 pt-6 my-6">
+            <div className="columns">
+              <div className="column is-two-thirds">
+                <section className="mt-6">
+                  <p className="has-text-white mx-2"><b>Ticket Price:</b> 20 USD</p>
+                  <button onClick={enterLotteryHandler} className="button is-link is-large is-link is-rounded mt-3">Buy Ticket</button>
+                </section>
+                <section className="mt-6">
+                  <p className="has-text-white mx-2"><b>Admin only:</b> Pick winner</p>
+                  <button onClick={pickWinnerHandler} className="button is-large is-link is-rounded mt-3">End Lottery</button>
+                </section>
+                <section>
+                  <div className="container has-text-danger mt-6">
+                    <p>{error}</p>
+                  </div>
+                </section>
+                <section>
+                  <div className="container has-text-success mt-6">
+                    <p>{successMsg}</p>
+                  </div>
+                </section>
+              </div>
+              <div className={`${styles.lotteryinfo} column is-one-third`}>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Recent Winner</h2>
+                        {
+                          (lotteryHistory && lotteryHistory.length > 0) && lotteryHistory.map(item => {
+                            if (lotteryId != item.id) {
+                              return <div className="history-entry mt-3" key={item.id}>
+                                <div>Lottery #{item.id} winner:</div>
+                                <div>
+                                  <a href={`https://etherscan.io/address/${item.address}`} target="_blank">
+                                    {item.address}
+                                  </a>
+                                </div>
+                              </div>
+                            }
+                          })
+                        }
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Players ({lotteryPlayers.length})</h2>
+                        <ul className="ml-0">
+                          {
+                            (lotteryPlayers && lotteryPlayers.length > 0) && lotteryPlayers.map((player, index) => {
+                              return <li key={`${player}-${index}`}>
+                                <a href={`https://etherscan.io/address/${player}`} target="_blank">
+                                  {player}
+                                </a>
+                              </li>
+                            })
+                          }
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Pot</h2>
+                        <p>{lotteryPot} Ether</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p className='has-text-white'>&copy; 2022 Team Semafour</p>
+      </footer>
+    </div>
+  )
+}
